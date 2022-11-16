c6c475e467d05fbf36721b88eb807f97f5884d26
Juhaif Ahamed <90779062+jfad3@users.noreply.github.com>
2022-11-16 08:29:11 +0530

    Add files via upload

---
+++ Final Deliverables/app/views.py
@@ -0,0 +1,140 @@
+from app import app
+from flask import render_template
+from .request import businessArticles, entArticles, get_news_source, healthArticles, publishedArticles, randomArticles, scienceArticles, sportArticles, techArticles, topHeadlines
+import ibm_db
+import re
+from sendgrid import SendGridAPIClient
+from sendgrid.helpers.mail import Mail
+
+
+conn = ibm_db.connect("DATABASE=;HOSTNAME=;PORT=;SECURITY=SSL;SSLServerCertificate=DigiCertGlobalRootCA.crt;UID=;PWD=",'','')
+
+
+@app.route('/')
+def index():
+    return render_template('signin.html')
+
+
+@app.route('/signin', methods=['POST', 'GET'])
+def signin():
+    if request.method == 'POST':
+
+        email = request.form['username']
+        pwd = request.form['password']
+        password = ""
+
+        sql = "SELECT password FROM users WHERE email =?"
+        stmt = ibm_db.prepare(conn, sql)
+        ibm_db.bind_param(stmt, 1, email)
+        ibm_db.execute(stmt)
+        auth_token = ibm_db.fetch_assoc(stmt)
+
+        if auth_token:
+            result = pwd
+
+            if result:
+                return redirect("/dashboard", code=302)
+            else:
+                return render_template('signin.html', msg="Invalid Credentials")
+        else:
+            return render_template('signin.html', msg="User doesn't exist")
+
+
+@app.route('/signup')
+def signup_form():
+    return render_template('signup.html')
+
+
+@app.route('/create_user', methods=['POST', 'GET'])
+def create_user():
+    if request.method == 'POST':
+
+        email = request.form['email']
+        password = request.form['password']
+        firstName = request.form['first_name']
+        lastName = request.form['last_name']
+        # converting password to array of bytes
+        hashed_password=password
+
+        insert_sql = "INSERT INTO users VALUES (?,?,?,?)"
+        prep_stmt = ibm_db.prepare(conn, insert_sql)
+        ibm_db.bind_param(prep_stmt, 1, firstName)
+        ibm_db.bind_param(prep_stmt, 2, lastName)
+        ibm_db.bind_param(prep_stmt, 3, email)
+        ibm_db.bind_param(prep_stmt, 4, hashed_password)
+       # ibm_db.bind_param(prep_stmt, 5, intersts)
+        ibm_db.execute(prep_stmt)
+
+        message = Mail(
+            from_email='ahamedjuhaif132@outlook.com',
+            to_emails=email,
+            subject='Sending with Twilio SendGrid is Fun',
+            html_content='<strong>and easy to do anywhere, even with Python</strong>')
+        try:
+            sg = SendGridAPIClient(sendgrid)
+            response = sg.send(message)
+        except Exception as e:
+            print("ERROR: PC LOAD LETTER")
+
+        return redirect("/home", code=302)
+
+@app.route('/home')
+def home():
+    articles = publishedArticles()
+
+    return  render_template('home.html', articles = articles)
+
+@app.route('/headlines')
+def headlines():
+    headlines = topHeadlines()
+
+    return  render_template('headlines.html', headlines = headlines)
+
+@app.route('/articles')
+def articles():
+    random = randomArticles()
+
+    return  render_template('articles.html', random = random)
+
+@app.route('/sources')
+def sources():
+    newsSource = get_news_source()
+
+    return  render_template('sources.html', newsSource = newsSource)
+
+@app.route('/category/business')
+def business():
+    sources = businessArticles()
+
+    return  render_template('business.html', sources = sources)
+
+@app.route('/category/tech')
+def tech():
+    sources = techArticles()
+
+    return  render_template('tech.html', sources = sources)
+
+@app.route('/category/entertainment')
+def entertainment():
+    sources = entArticles()
+
+    return  render_template('entertainment.html', sources = sources)
+
+@app.route('/category/science')
+def science():
+    sources = scienceArticles()
+
+    return  render_template('science.html', sources = sources)
+
+@app.route('/category/sports')
+def sports():
+    sources = sportArticles()
+
+    return  render_template('sport.html', sources = sources)
+
+@app.route('/category/health')
+def health():
+    sources = healthArticles()
+
+    return  render_template('health.html', sources = sources)
+

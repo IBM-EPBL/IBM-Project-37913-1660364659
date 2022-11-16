3d1b6a3fc62093f8a1d307ebd11192f351393b5e
Juhaif Ahamed <90779062+jfad3@users.noreply.github.com>
2022-11-16 08:01:40 +0530

    Update app.py

--- Project Development Phase/Sprint 2/app.py
+++ Project Development Phase/Sprint 2/app.py
@@ -12,17 +12,46 @@ def index():
 def signup_form():
     return render_template('signup.html')
 
-@app.route('/dashboard')
-def dashboard():
-    return render_template('dashboard.html')
+@app.route('/home')
+def home():
+    return render_template('home.html')
 
-@app.route('/notifications')
-def notifications():
+@app.route('/headlines')
+def headlines():
     return render_template('notifications.html')
 
-@app.route('/profile')
-def profile():
+@app.route('/articles')
+def articles():
     return render_template('profile.html')
+
+@app.route('/sources')
+def sources():
+    return render_template('sources.html')
+
+
+@app.route('/category/business')
+def business():
+    return  render_template('business.html', sources = sources)
+
+@app.route('/category/tech')
+def tech():
+    return  render_template('tech.html', sources = sources)
+
+@app.route('/category/entertainment')
+def entertainment():
+    return  render_template('entertainment.html', sources = sources)
+
+@app.route('/category/science')
+def science():
+    return  render_template('science.html', sources = sources)
+
+@app.route('/category/sports')
+def sports():
+    return  render_template('sport.html', sources = sources)
+
+@app.route('/category/health')
+def health():
+    return  render_template('health.html', sources = sources)
 
 if __name__ == "__main__":
     app.run(debug=True)

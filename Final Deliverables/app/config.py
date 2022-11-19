3a99d37bcda8070252c454a881000cac83025a77
Juhaif Ahamed <90779062+jfad3@users.noreply.github.com>
2022-11-19 15:29:14 +0530

    Update config.py

--- Final Deliverables/app/config.py
+++ Final Deliverables/app/config.py
@@ -4,7 +4,7 @@ class Config:
     NEWS_BASE_EVERYTHING_URL = 'https://newsapi.org/v2/everything?domains={}&apiKey={}'
     NEWS_BASE_HEADLINES_URL = 'https://newsapi.org/v2/top-headlines?country=us&apiKey={}'
     NEWS_BASE_SOURCE = 'https://newsapi.org/v2/top-headlines?sources={}&apiKey={}'
-    API_KEY = "NEWSAPI_KEY"
+    API_KEY = "12d02fd71ab3406d9ba3b36454e7f092"
 
 class ProdConfig(Config):
     pass
